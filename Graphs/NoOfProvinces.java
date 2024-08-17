dc9bfcb7f18ea9c50111ed711b88532d56e56577
idealisticgerm <siddheshkaskar5678@gmail.com>
2024-08-17 21:02:14 +0530

    GraphsDFS

--- Graphs/NoOfProvinces.java
+++ Graphs/NoOfProvinces.java
@@ -1,52 +1,46 @@
 package Graphs;
 import java.util.*;
 
+// Leetcode 547(https://leetcode.com/problems/number-of-provinces)
 public class NoOfProvinces {
-    public static void dfs(ArrayList<ArrayList<Integer>> ls,boolean[] visited,int node){
-//        int n = ls.size();
+
+    public static void dfs(ArrayList<ArrayList<Integer>> ls, boolean[] visited, int node) {
         visited[node] = true;
-        for (Integer it:ls.get(node)){
-            if(visited[it] == false){
-                dfs(ls,visited,it);
+        for (Integer it : ls.get(node)) {
+            if (visited[it] == false) {
+                dfs(ls, visited, it);
             }
         }
-
-
     }
 
-
-
-    public static int  findCircleNum(int[][] isConnected) {
-        int V =isConnected.length;
+    public static int findCircleNum(int[][] isConnected) {
+        int V = isConnected.length;
         ArrayList<ArrayList<Integer>> ls = new ArrayList<>();
         boolean[] visited = new boolean[V];
 
-        for(int i =0;i < V; i++){
+        // Converting matrix to ArrayList
+        for (int i = 0; i < V; i++) {
             ArrayList<Integer> rowList = new ArrayList<>();
-            for(int j =0;j< isConnected[0].length;j++ ){
-                if(isConnected[i][j] == 1 && i!=j){
+            for (int j = 0; j < isConnected[0].length; j++) {
+                if (isConnected[i][j] == 1 && i != j) {
                     rowList.add(j);
                 }
             }
             ls.add(rowList);
         }
-//        System.out.println(ls);
-    int count = 0;
-        for (int i = 0;i < V;i++){
-        if(visited[i] == false){
-            count++;
-            dfs(ls,visited,i);
-        }
+        int count = 0; // to count how many times if condition runs
+        for (int i = 0; i < V; i++) {
+            if (visited[i] == false) {
+                count++;
+                dfs(ls, visited, i);
+            }
         }
         return count;
     }
 
     public static void main(String[] args) {
-        int[][] isConnected = {{1,1,0},{1,1,0},{0,0,1}};
-//        boolean[] visited = new boolean[isConnected.length];
-
+        int[][] isConnected = {{1, 1, 0}, {1, 1, 0}, {0, 0, 1}};
         int count = findCircleNum(isConnected);
-
         System.out.println(count);
     }
 }
